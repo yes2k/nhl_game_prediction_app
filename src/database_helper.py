8a2a62916f9864e56b776d1f5d046fab0ed49120
Yatharth Khattar <yatharthkhattar@gmail.com>
2024-10-19 23:32:29 -0400

    added function to update database

--- src/database_helper.py
+++ src/database_helper.py
@@ -1,7 +1,7 @@
 import polars as pl
 import sqlite3
 import requests
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 import sys
 import time
 from argparse import ArgumentParser
@@ -29,14 +29,15 @@ def get_reg_goals(date: str):
 
         return out
 
+    
 
     url = f"https://api-web.nhle.com/v1/score/{date}"
     try:
         data = requests.get(url).json()
     except:
         print("url not found")
-    
-    # print(url)
+
+
 
     out = []
     for game in data["games"]:
@@ -46,8 +47,6 @@ def get_reg_goals(date: str):
     return out
 
 
-
-
 def build_database(start_date: str, path_to_db: str) -> None:
     """
     start_date should be formatted as YYYY-MM-DD    
@@ -55,11 +54,6 @@ def build_database(start_date: str, path_to_db: str) -> None:
     start_date = date(int(start_date[0:4]), int(start_date[5:7]), int(start_date[8:10]))
     end_date = date.today() - timedelta(days = 1)
     date_range = pl.date_range(start_date, end_date, eager=True).cast(pl.String).alias("date").to_list()
-    
-    # try: 
-    # conn = sqlite3.connect("data.db") 
-    # except: 
-    #     raise FileNotFoundError("Can't create database")
     
 
     t0 = time.time()
@@ -72,14 +66,43 @@ def build_database(start_date: str, path_to_db: str) -> None:
 
     df.write_database(
         table_name = "goal_data",
-        connection = f"sqlite://{path_to_db}//data.db",
+        connection = f"sqlite:///{path_to_db}/data.db",
         if_table_exists="replace"
     )
 
 
 
 def update_database(path_to_db: str) -> None:
-    pass
+    con = sqlite3.connect(f"{path_to_db}/data.db")
+    cur = con.cursor()
+    max_date = cur.execute("SELECT MAX(date) FROM goal_data").fetchall()[0][0]
+    con.close()
+
+    max_date = datetime.strptime(max_date, "%Y-%m-%d")
+
+    date_range = (
+        pl
+        .date_range(
+            max_date + timedelta(days = 1), 
+            date.today() - timedelta(days = 1), 
+            eager=True
+        )
+        .cast(pl.String)
+        .alias("date").to_list()
+    )
+
+    out = []
+    for d in date_range:
+        out.append(pl.DataFrame(get_reg_goals(d)))
+    
+    df = pl.concat(out, how = "diagonal")
+    df.write_database(
+        table_name = "goal_data",
+        connection = f"sqlite:///{path_to_db}/data.db",
+        if_table_exists="append"
+    )
+    
+
 
 
 if __name__  == "__main__":
@@ -103,7 +126,3 @@ if __name__  == "__main__":
             update_database(args.pathtodb)
         case _: ValueError("Incorrect argument for type, takes one of 'rebuild' or 'update'")
 
-
-
-    # out = build_database("2023-10-04", "")
-    # print(out)
