73319e4fffe04b51d43bf7840791934348654ff2
Yatharth Khattar <yatharthkhattar@gmail.com>
2024-10-26 17:18:46 -0400

    rewrote some api

--- src/api.py
+++ src/api.py
@@ -13,56 +13,7 @@ from model import fit_model, get_table_of_predictions
 templates = Jinja2Templates(directory="templates")
 
 
-
-
-
-app = FastAPI()
-
-@app.get("/", response_class=HTMLResponse)
-async def root(request: Request):
-    return templates.TemplateResponse(request=request, name="main.html")
-
-
-
-@app.get("/game/{date_of_pred}")
-async def get_predictions(date_of_pred: str, home_team: str, away_team: str):
-    # Placeholder for the actual model fitting and prediction logic
-
-    res = await get_game_ids(date)
-    if len(res) == 0:
-        return {
-            "error": "No games found"
-        }
-    else:
-        print(res)
-        season = res[0]["season"]
-
-    max_date = datetime.strptime(
-        date(int(date_of_pred[0:4]), int(date_of_pred[5:7]), int(date_of_pred[8:10])) - timedelta(days=1), 
-        "%Y-%m-%d"
-    )
-
-    model_out = fit_model(
-        "model/model.stan",
-        "data/data.db",
-        home_team=home_team,
-        away_team=away_team,
-        season=season,
-        max_date=max_date
-    )
-
-    return get_table_of_predictions(model_out).as_dicts()
-
-
-
-@app.get("/team_params/{game_id}")
-async def get_team_params(team_id: int):
-    pass
-
-
-
-@app.get("/game_ids/{date}")
-async def get_game_ids(date: str):
+def get_game_ids(date: str):
     url = f"https://api-web.nhle.com/v1/schedule/{date}"
     
     try:
@@ -84,3 +35,57 @@ async def get_game_ids(date: str):
         })
 
     return out
+
+
+
+
+
+
+
+app = FastAPI()
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse(request=request, name="main.html")
+
+
+
+@app.get("/game/{date_of_pred}")
+async def get_predictions(date_of_pred: str, home_team: str, away_team: str):
+
+    res = get_game_ids(date_of_pred)
+    if len(res) == 0:
+        return {
+            "error": "No games found"
+        }
+    else:
+        season = str(res[0]["season"])[:4]
+
+    max_date = (
+        datetime.strptime(date_of_pred, "%Y-%m-%d") - timedelta(days=1)
+    ).strftime("%Y-%m-%d")
+
+    print(season)
+
+    model_out = fit_model(
+        "src/model/model.stan",
+        "data/data.db",
+        home_team=home_team,
+        away_team=away_team,
+        season=season,
+        max_date=max_date
+    )
+
+    return get_table_of_predictions(model_out)
+
+
+
+@app.get("/team_params/{game_id}")
+async def get_team_params(team_id: int):
+    pass
+
+
+
+@app.get("/game_ids/{date}")
+async def get_all_games(date: str):
+    return get_game_ids(date)
