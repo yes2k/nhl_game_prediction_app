ce70d30bd5b4cec40fbb49b574b82cbe8472a4a4
Yatharth Khattar <yatharthkhattar@gmail.com>
2025-02-08 15:58:10 -0500

    added api functions to get season proj data and plots

--- src/api.py
+++ src/api.py
@@ -29,24 +29,28 @@ async def root(request: Request):
 
 @app.get("/game/{date_of_pred}")
 async def get_predictions(date_of_pred: str, home_team: str, away_team: str):
-    res = helper.get_game_ids(date_of_pred)
-    if len(res) == 0:
-        return {
-            "error": "No games found"
-        }
-    else:
-        season = str(res['res'][0]["season"])[:4]
-
+    season = helper.get_nhl_season(date_of_pred)
     out = Mod.get_prediction(date_of_pred, season, home_team, away_team)
-
-    print(out)
-
     return { 
         'table_of_pred': out.pred_table.to_dicts(), 
         'team_params': out.team_params.to_dicts(),
         'home_team_win_prob': out.prob_home_team_win * 100
     }
 
+
+@app.get("/season_projection")
+async def get_season_projection():
+    return Mod.get_season_prediction()
+
+@app.get("/season_projection_plot")
+async def get_season_projection_plot():
+    return Mod.get_season_projection_box_plot()
+
+
+@app.get("/game/{date_of_pred}/heatmap")
+async def get_heatmap(date_of_pred: str, home_team: str, away_team: str):
+    season = helper.get_nhl_season(date_of_pred)
+    return Mod.get_prediction_heatmap_html(date_of_pred, season, home_team, away_team)
 
 
 @app.get("/team_params/{game_id}")
